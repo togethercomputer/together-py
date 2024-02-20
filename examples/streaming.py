2d56f586e64598fe014177392c0b6ccb51abca1e
Robert Craigie <robert@craigie.dev>
2024-02-20 18:20:01 +0000

    Add streaming example

---
+++ examples/streaming.py
@@ -0,0 +1,19 @@
+from together_ai import TogetherAI
+
+client = TogetherAI()
+
+stream = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    stream=True,
+)
+for chunk in stream:
+    choice = chunk.choices[0]
+    if choice.delta.content:
+        print(choice.delta.content, end="", flush=True)
+print()
