6502acc911413abceff3870f620a2bed742e9b08
Blaine Kasten <blainekasten@gmail.com>
2026-02-19 07:20:47 -0600

    chore(cli): Improve messaging when attempting to cancel finetune that is not cancellable (#268)

--- src/together/lib/cli/api/fine_tuning/cancel.py
+++ src/together/lib/cli/api/fine_tuning/cancel.py
@@ -6,6 +6,7 @@ from together import Together
 from together.lib.cli.api._utils import handle_api_errors
 from together.lib.utils.serializer import datetime_serializer
 
+NON_CANCELLABLE_STATES = ["cancel_requested", "cancelled", "error", "completed", "user_error"]
 
 @click.command()
 @click.pass_context
@@ -15,6 +16,11 @@ from together.lib.utils.serializer import datetime_serializer
 def cancel(ctx: click.Context, fine_tune_id: str, quiet: bool = False) -> None:
     """Cancel fine-tuning job"""
     client: Together = ctx.obj
+    job = client.fine_tuning.retrieve(fine_tune_id)
+    if job.status in NON_CANCELLABLE_STATES:
+        click.echo(click.style(f"Fine-tuning: ", fg="blue") + f"Training is not currently cancellable. Current status is " + click.style(job.status, fg="yellow"))
+        return
+
     if not quiet:
         confirm_response = input(
             "You will be billed for any completed training steps upon cancellation. "
@@ -26,3 +32,4 @@ def cancel(ctx: click.Context, fine_tune_id: str, quiet: bool = False) -> None:
     response = client.fine_tuning.cancel(fine_tune_id)
 
     click.echo(json.dumps(response.model_dump(exclude_none=True), indent=4, default=datetime_serializer))
+
