a6f38eaa590dd2211478381caf1e93130b55a65f
Blaine Kasten <blainekasten@gmail.com>
2026-02-18 20:59:07 -0600

    Update src/together/lib/cli/api/_utils.py
    Co-authored-by: technillogue <technillogue@gmail.com>

--- src/together/lib/cli/api/_utils.py
+++ src/together/lib/cli/api/_utils.py
@@ -148,10 +148,8 @@ def handle_api_errors(prefix: str) -> Callable[[F], F]:
             try:
                 return f(*args, **kwargs)
             # User aborted the command
-            except click.Abort:
-                sys.exit(0)
-            # Re-raise a usage error so it displays a proper click error message
-            except click.UsageError as e:
+            # Re-raise abort and usage errore so it displays a proper click message
+            except (click.Abort, click.UsageError) as e:
                 raise e
             except APIError as e:
                 error_msg = ""
