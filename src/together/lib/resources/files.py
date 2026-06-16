78098cb4f4ea92f048c2f58f7ab7ae9b2eedc4b7
Ruslan Khaidurov <rkhaidurov@together.ai>
2026-06-16 16:30:07 +0100

    MOSH-2945: raise if file exists (#397)
    * Update files.py
    
    * Update files.py

--- src/together/lib/resources/files.py
+++ src/together/lib/resources/files.py
@@ -644,7 +644,7 @@ class MultipartUploadManager(SyncAPIResource):
             return self._complete_upload(url, upload_id, file_id, completed_parts)
 
         # If the server says the file already exists, raise the error to the files.upload resource
-        # This should be silently handled by fetching down the file and returning it
+        # This should be loudly handled by raising a ValueError and outputting the existing file ID
         except FileAlreadyExistsError as e:
             raise e
         except Exception as e:
@@ -1051,7 +1051,7 @@ class AsyncMultipartUploadManager(AsyncAPIResource):
             return await self._complete_upload(url, upload_id, file_id, completed_parts)
 
         # If the server says the file already exists, raise the error to the files.upload resource
-        # This should be silently handled by fetching down the file and returning it
+        # This should be loudly handled by raising a ValueError and outputting the existing file ID
         except FileAlreadyExistsError as e:
             raise e
         except Exception as e:
